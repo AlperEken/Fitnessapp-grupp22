5dc6183a10c97e15fdf4e1bea7fe4012d5c637bc
AlperEken <alpereken04@gmail.com>
2025-04-02 21:03:26 +0200

    Fixade error med bilden i framsidan

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,8 @@
 package controller;
+
 import view.StartScreen;
+
+
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> new StartScreen());
